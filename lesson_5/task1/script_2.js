16c222d4b7dabcfdd9736a338f61e3033204b285
NShevtsov <neketobro@gamil.com>
2024-11-19 20:28:01 +0200

    task5.2 comlited

--- lesson_5/task1/script_2.js
+++ lesson_5/task1/script_2.js
@@ -1,50 +1,45 @@
-// Обрахувати валюту де один долар коштує 26 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів
-
-// Приклад:
-
-// 10$ = 260₴
-// 20$ = 540₴
-// За допомогою prompt() запитати від користувача валюту (Наприклад: uah, usd, euro).
-
-
 let userCurrency = prompt('Яку валюту Ви хочете розрахувати?', '');
 let currency = '';
 
 let uah = 0.024;
-let usd = 41;
-let euro = 47;
+let usd = 41.29;
+let euro = 43.61;
 
-switch (userCurrency.toLocaleLowerCase()) {
-    case 'uah':
-    case 'гривня':
-    case 'гривна':
-    case '₴':
-        currency = uah;
-        break;
-    case 'usd':
-    case 'долар':
-    case 'dollar':
-    case '$':
-        currency = usd;
-        break;
-    case 'euro':
-    case 'евро':
-    case 'євро':
-    case '€':
-        currency = euro;
-        break;
-    default:
-        console.log('not value');
-        break;
-};
+let invalidValue = false;
 
-console.log(currency);
+if (!userCurrency) userCurrency = '';
 
 let i = 0;
 while (i < 100) {
     i++;
     if (i % 10 === 0) {
-        console.log(i + '$' + ' = ' + (i * currency) + '₴');
+        switch (userCurrency.toLocaleLowerCase()) { 
+            case 'uah':    
+            case 'гривня':
+            case 'гривна':
+            case 'hryvnia':
+            case '₴':
+                console.log(i + '₴' + ' = ' + (i * uah) + '$');
+                break;
+            case 'usd':
+            case 'долар':
+            case 'dollar':
+            case '$':
+                console.log(i + '$' + ' = ' + Math.round(i * usd) + '₴');
+                break;
+            case 'euro':
+            case 'евро':
+            case 'євро':
+            case '€':
+                console.log(i + '€' + ' = ' + Math.round(i * euro) + '₴');
+                break;
+            default:
+                invalidValue = true;
+                break;
+        };
     };
+    
+    if (invalidValue == true) alert('На жаль введену валюти не розіпнано, \nВи можете ввести - "USD", "UAH", "EURO".');
+    if (invalidValue == true) break;
 };
 
