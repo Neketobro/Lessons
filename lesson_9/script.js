b3023a9e300ef1a3a723fbbec16d9dacf718c3bc
NShevtsov <neketobro@gamil.com>
2024-12-05 23:06:21 +0200

    copy condition

---
+++ lesson_9/script.js
@@ -0,0 +1,30 @@
+// Сумма зарплат
+// Дізнатись суму всіх зарплат користувачів:
+
+const conmpany = {
+    sales: [
+        { name: 'Jhon', salary: 1000 }, 
+        { name: 'Alice', salary: 600 },
+        { name: 'Bob', salary: 1200 },
+    ],
+    development: {
+        web: [
+            { name: 'Peter', salary: 2000 },
+            { name: 'Alex', salary: 1800 },
+            { name: 'Max', salary: 600 },
+        ],
+        internals: [ 
+            { name: 'Jack', salary: 1300 },
+        ]
+    }
+}
+
+function totalSales(company) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+    }
+}
+
+const total = totalSales(conmpany);
+console.log(total);
+// Об'єкт може містити невідому кількість департаментів та співробітників
