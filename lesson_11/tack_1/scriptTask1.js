2b1d8ca05d469c4247dae558ed0bf54fb1691178
NShevtsov <neketobro@gamil.com>
2024-12-13 00:50:33 +0200

    complate lesson 11

--- lesson_11/tack_1/scriptTask1.js
+++ lesson_11/tack_1/scriptTask1.js
@@ -1,43 +1,40 @@
-// // # HW 11.1
+document.body.style.display = 'flex';
 
-// // ## Таблиця піфагора
+function sum() {
+    let result = 1;
 
-// // Вивести таблицю Піфагора (10×10), проте цього разу таблиця повинна бути створена динамічно
-
-
-// function sum() {
-//     let result = 1;
-    
-//     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-//     for (let i = 1; i <= 10; i++){ 
-//         for (let j = 1; j <= 10; j++) {
-//             result = i * j;
-//             // console.log(result);
-//         }
-//     }
-// }
-// sum();
-
-
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (let i = 1; i <= 10; i++){ 
+        const fatherDiv = document.createElement('div');
+        fatherDiv.classList.add('father');
+        
+        for (let j = 1; j <= 10; j++) {
+            result = i * j;
+            
+            fatherDiv.insertAdjacentElement('beforeend', createList(result));
+        }
+        document.body.insertAdjacentElement('beforeend', fatherDiv);
+    }
+}
+sum();
 
 function createListEl(data) {
-    const el = document.createElement('p');
+    const el = document.createElement('div');
     el.innerText = data;
+    el.style.textAlign = 'center';
 
     return el;
-};
+}
 
 function createList(data) {
     const div = document.createElement('div');
 
-    data.forEach( el => {
-        div.insertAdjacentElement('beforeend', createListEl(el))
-    });
+    div.insertAdjacentElement('beforeend', createListEl(data))
+
+    div.style.display = 'flexbox';
+    div.style.width = '30px';
+    div.style.height = '30px';
+    div.style.background = 'grey';
+    div.style.border = '1px solid black';
 
     return div;
-};
-
-document.body.insertAdjacentElement('beforeend', createList(data));
+}
