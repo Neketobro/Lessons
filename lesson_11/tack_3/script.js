97b8227f3106423b36673a4dbfcae5a96f91f561
NShevtsov <neketobro@gamil.com>
2024-12-13 00:58:19 +0200

    update code for lesson 11 and rename folder

--- lesson_11/tack_3/script.js
+++ lesson_11/tack_3/script.js
@@ -3,11 +3,7 @@ const btn = document.getElementById('button');
 btn.addEventListener('click', () => {    
     btn.previousElementSibling.classList.toggle('paragraf');
 
-    if (btn.previousElementSibling.classList.contains('paragraf')) {
-        btn.previousElementSibling.style.color = 'red';
-    } else {
-        btn.previousElementSibling.style.color = 'black';
-    }
+    btn.previousElementSibling.classList.contains('paragraf') ? btn.previousElementSibling.style.color = 'red' : btn.previousElementSibling.style.color = 'black';
 });
 
 
