41e1c8d0d2bb88290056f5973db8c6b56aaa0518
NShevtsov <neketobro@gamil.com>
2024-12-20 15:48:39 +0200

    fun2

--- lesson_13/script.js
+++ lesson_13/script.js
@@ -1,12 +1,11 @@
 const formElements = document.forms.form.elements;
 
-// formElements.name.addEventListener('change', (e) => {
-//     if (!e.target.value.trim()) console.log('value undefind');
-// })
-
 document.forms.form.addEventListener('focusout', (e) => {
     e.preventDefault();
-    //name
+    checkInput(e);
+});
+
+function checkInput(e) {
     if (e.target.name === 'name' && !e.target.value.trim()) {
         e.target.previousElementSibling.textContent = 'Name is not valid';
         e.target.previousElementSibling.style.color = 'red';
@@ -16,9 +15,8 @@ document.forms.form.addEventListener('focusout', (e) => {
     } else {
         e.target.previousElementSibling.textContent = '';
         e.target.style.border = 'none';
-    }
+    };
 
-    //textArea
     const regTextArea = /\w{5}/m;
     if (e.target.name === 'textArea' && !regTextArea.test(e.target.value)) { 
         e.target.previousElementSibling.textContent = 'Message must contain at least one word with 5 characters';
@@ -30,9 +28,8 @@ document.forms.form.addEventListener('focusout', (e) => {
     else {
         e.target.previousElementSibling.textContent = '';
         e.target.style.border = 'none';
-    }
+    };
 
-    // phone number
     const regNumber = /^\+380\d{9,9}$/ig;
     if (e.target.name === 'phoneNumber' && !regNumber.test(e.target.value)) {
         e.target.previousElementSibling.textContent = 'Entered number is incorrect. Number must be "+380" and have 9 digits.';
@@ -43,10 +40,8 @@ document.forms.form.addEventListener('focusout', (e) => {
     } else {
         e.target.previousElementSibling.textContent = '';
         e.target.style.border = 'none';
-    }
-    
+    };
 
-    // emeil
     const regEmail = /\w+@\w+\.\w{2,3}/g;
     if (e.target.name === 'email' && !regEmail.test(e.target.value)) {
         e.target.previousElementSibling.textContent = 'Email is not valid';
@@ -55,32 +50,31 @@ document.forms.form.addEventListener('focusout', (e) => {
 
         return false;
     } else {
-        console.log('valid email');
         e.target.previousElementSibling.textContent = '';
         e.target.style.border = 'none';
-    }
+    };
+};
 
-
-
-
-    if (e.target.name) console.log(e.target.value);
-});
-
-
-
-///submit 
 document.forms.form.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    // checkInput(e);
+
     fooSubmit(e);
 });
 
 function fooSubmit(e) {
+    // checkInput(e);
     const result = {};
     
     Array.from(e.target.elements).forEach(el => {
         if (el.name) {
             result[el.name] = el.value;
-        }
+
+            // if (!el.value) {
+            //     return checkInput(el.name);
+            // };
+        };
     });
     
     console.log(result)
