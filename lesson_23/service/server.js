310d22b3f868a6211ea6ab6911594734aece4b13
NShevtsov <neketobro@gamil.com>
2025-02-03 18:22:20 +0200

    add express

--- lesson_23/service/server.js
+++ lesson_23/service/server.js
@@ -1,72 +1,40 @@
-import mongoose from "mongoose";
-import { Logger } from "./libs/logger.js";
-import { AppConfig } from "./config/app.config.js";
-import { app } from "./src/index.js";
-
-const { port, hostname } = AppConfig.server;
-const { connectionString } = AppConfig.db;
-
-mongoose.connect(connectionString);
-
-const db = mongoose.connection;
-
-db.on("error", (error) => {
-    Logger.error(`Connection error: ${JSON.stringify(error)}`);
-});
-db.on("open", () => {
-    Logger.info("Connected to MongoDB is opened 🐾🐾🐾 ");
-});
-
-app.listen(port, hostname, () => {
-    Logger.info(`Server is serving on http://${hostname}:${port}`);
-    Logger.info(
-        `Swagger documentation is serving on http://${hostname}:${port}/api`,
-    );
-});
-
-
-
-
-
-
-
-
+const { MongoClient } = require("mongodb");
 // Replace the following with your Atlas connection string                                                                                                                                        
-// const url = "mongodb+srv://user:Osi0AraDRpZj4HxL@usernikita.r7ea6.mongodb.net/?retryWrites=true&w=majority&appName=UserNikita";
+const url = "mongodb+srv://user:Osi0AraDRpZj4HxL@usernikita.r7ea6.mongodb.net/?retryWrites=true&w=majority&appName=UserNikita";
 
-// // Connect to your Atlas cluster
-// const client = new MongoClient(url);
+// Connect to your Atlas cluster
+const client = new MongoClient(url);
 
-// async function run() {
-//     try {
-//         await client.connect();
-//         console.log("Successfully connected to Atlas");
+async function run() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to Atlas");
 
-//         const db = client.db("TODO");
-//         const col = db.collection("TODOs");
+        const db = client.db("TODO");
+        const col = db.collection("TODOs");
 
-//         const todosDocument = [
-//             {
-//                 "name": { "first": "Alan", "last": "Turing" },
-//                 "birth": new Date(1912, 5, 23), // May 23, 1912                                                                                                                                 
-//                 "death": new Date(1954, 5, 7),  // May 7, 1954                                                                                                                                  
-//                 "contribs": ["Turing machine", "Turing test", "Turingery"],
-//                 "views": 1250000
-//             }
-//         ]
-//         // Insert the documents into the specified collection        
-//         const p = await col.insertMany(todosDocument);
-//         // Find the document
-//         const filter = { "name.last": "Turing" };
-//         const document = await col.findOne(filter);
-//         // Print results
-//         console.log("Document found:\n" + JSON.stringify(document));
-//     } catch (err) {
-//         console.log(err.stack);
-//     }
-//     finally {
-//         await client.close();
-//     }
-// }
+        const todosDocument = [
+            {
+                "name": { "first": "Alan", "last": "Turing" },
+                "birth": new Date(1912, 5, 23), // May 23, 1912                                                                                                                                 
+                "death": new Date(1954, 5, 7),  // May 7, 1954                                                                                                                                  
+                "contribs": ["Turing machine", "Turing test", "Turingery"],
+                "views": 1250000
+            }
+        ]
+        // Insert the documents into the specified collection        
+        const p = await col.insertMany(todosDocument);
+        // Find the document
+        const filter = { "name.last": "Turing" };
+        const document = await col.findOne(filter);
+        // Print results
+        console.log("Document found:\n" + JSON.stringify(document));
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
 
-// run().catch(console.dir);
+run().catch(console.dir);
