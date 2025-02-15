de27ec4e743199f8223791d667aa33d23bfe905b
NShevtsov <neketobro@gamil.com>
2025-02-15 16:58:59 +0200

    creatr folder for lesson_26

---
+++ lesson_26/my-app/src/components/HandlerEmojy/HandlerEmojy.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './HandlerEmojy.css'
+
+export function HandlerEmojy() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Голосуй за найкращий смайлик</h1>
+    </>
+  )
+}
