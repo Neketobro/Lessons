485419018f2a27f06bc1636955d319a92a1002ce
NShevtsov <neketobro@gamil.com>
2025-02-16 14:21:24 +0200

    complate home work in lesson 26

--- lesson_26/my-app/src/App.jsx
+++ lesson_26/my-app/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { HandlerEmojy } from "./components";
 
 import './App.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <HandlerEmojy />
