e7115d97d75e885064301251ecd8e5845a780665
NShevtsov <neketobro@gamil.com>
2025-02-18 16:26:47 +0200

    add file in folders

--- lesson_27/my-react-app/src/App.jsx
+++ lesson_27/my-react-app/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/index'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
+    <RouterProvider
+      router={router}
+      fallbackElement={'Error'} // <Loader />
+      future={{ v7_startTransition: true }}
+    />
   )
 }
-
-export default App
+export default App;
--- lesson_27/my-react-app/src/components/Footer/Footer.jsx
+++ lesson_27/my-react-app/src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-export function Header() {
+export function Footer() {
 
     return (
         <>
