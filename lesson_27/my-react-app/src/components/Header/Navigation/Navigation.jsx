e24f0e976aac56119025a759487408f4737aa295
NShevtsov <neketobro@gamil.com>
2025-02-19 00:13:09 +0200

    creatr wrapper header & add logo

---
+++ lesson_27/my-react-app/src/components/Header/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+// import style from './Navigation.scss';
+import './Navigation.scss';
+
+export function Navigation() {
+
+    return (
+        <div className="navigation-panel">
+            <div>NavPanel</div>
+        </div>
+    )
+}
