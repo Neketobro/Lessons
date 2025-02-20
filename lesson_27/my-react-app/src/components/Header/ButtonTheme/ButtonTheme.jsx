7f515c25a7b605c84b9ed3656831df363422a616
NShevtsov <neketobro@gamil.com>
2025-02-20 11:38:29 +0200

    context fail

--- lesson_27/my-react-app/src/components/Header/ButtonTheme/ButtonTheme.jsx
+++ lesson_27/my-react-app/src/components/Header/ButtonTheme/ButtonTheme.jsx
@@ -7,7 +7,13 @@ export function ButtonTheme() {
     const { theme, setTheme } = useContext(themeContext);
     const content = theme === "dark" ? "light" : "dark";
 
+
+    console.log(content);
+    console.log("Тема у ButtonTheme:", theme);
+
+
     const clickHandler = () => {
+        console.log('clicked');
         setTheme((prevTheme) =>
             prevTheme === THEME_DARK ? THEME_LIGHT : THEME_DARK,
         );
