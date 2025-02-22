2c0b6ed854d5b31f54625f7f532e0ec70e3444d4
NShevtsov <neketobro@gamil.com>
2025-02-22 11:48:11 +0200

    creatr some Form and add YUP

--- lesson_28/my-react-app/src/components/Todos/Todos.jsx
+++ lesson_28/my-react-app/src/components/Todos/Todos.jsx
@@ -1,7 +1,25 @@
+import { Field, Formik, Form, ErrorMessage } from 'formik';
+import { todosScheme } from '../../services'
+
 export function Todos() {
     return (
         <div>
-            <h1>Todos</h1>
+            <Formik
+                initialValues={{ todoText: '' }}
+                onSubmit={(values, formikHelper) => {
+                    setTimeout(() => {
+                        console.log(values);
+                        formikHelper.resetForm();
+                        formikHelper.setSubmitting(false)
+                    }, 1000)
+                }}
+                validationSchema={todosScheme}>
+                <Form>
+                    <Field type="text" name="todoText" placeholder="Enter your text..." />
+                    <ErrorMessage name='todoText' />
+                    <button type='submit'>Send</button>
+                </Form>
+            </Formik>
         </div>
     )
 }
