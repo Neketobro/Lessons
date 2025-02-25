d1947d3b8cc0376ced107d5345a14fbbb54e9952
NShevtsov <neketobro@gamil.com>
2025-02-26 00:31:20 +0200

    add redux

--- lesson_29/my-react-app/src/pages/Counter/index.jsx
+++ lesson_29/my-react-app/src/pages/Counter/index.jsx
@@ -1,13 +1,20 @@
 import { Footer, Header, PageLayout } from '../../components';
+import { useDispatch } from "react-redux";
+import { incremented, decremented, reset } from "../../ducks/counter.duck.js";
 
 
 export function CounterPage() {
+    const dispatch = useDispatch();
+
     return (
         <PageLayout
             renderHeader={() => <Header />}
             renderMain={() => (
                 <div>
-                    <h4>Counter</h4>
+                    <p>Value: 0</p>
+                    <button onClick={() => dispatch(incremented())}>+</button>
+                    <button onClick={() => dispatch(decremented())}>-</button>
+                    <button onClick={() => dispatch(reset())}>reset</button>
                 </div>
             )}
             renderFooter={() => <Footer />}
