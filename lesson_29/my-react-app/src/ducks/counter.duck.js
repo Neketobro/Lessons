76f219f608bd22ccc94d6bdc2c1509d308220ab3
NShevtsov <neketobro@gamil.com>
2025-02-26 08:20:29 +0200

    creatr counter

--- lesson_29/my-react-app/src/ducks/counter.duck.js
+++ lesson_29/my-react-app/src/ducks/counter.duck.js
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     value: 0
@@ -7,12 +7,22 @@ const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
-        incremented: (state) => state.value += 1,
-        decremented: (state) => state.value >= 0 ? state.value += 1 : state,
-        reset: (state) => state.value = initialState.value,
+        incremented: (state) => {
+            state.value = state.value + 1
+        },
+        decremented: (state) => {
+            state.value > 0 ? state.value = state.value - 1 : state.value
+        }, 
+        reset: (state) => {
+            state.value = initialState.value
+        },
     },
+    selectors: {
+        selectCounter: (state) => state.value,
+    }
 })
 
-export const { incremented, decremented, reset } = counterSlice.actions
+export const { incremented, decremented, reset } = counterSlice.actions;
+export const { selectCounter } = counterSlice.selectors;
 
 export default counterSlice.reducer;
