a759c95f38b7341132b3c7e8c8d4ef1bf8ae3087
NShevtsov <neketobro@gamil.com>
2025-03-07 17:02:14 +0200

    add contact in footer

--- lesson_32/my-app/src/components/Footer/Footer.jsx
+++ lesson_32/my-app/src/components/Footer/Footer.jsx
@@ -1,7 +1,10 @@
+import { Box } from '@mui/material';
+
 export function Footer() {
     return (
-        <>
-            <h2>Footer</h2>
-        </>
+        <Box>
+            <Box component='h4' sx={{ margin: '0' }}>My contact:</Box>
+            <Box component='p' sx={{ margin: '0' }}>Email: NShevtsov300404@gmail.com</Box>
+        </Box>
     )
 }
