b913a621b69edbc7df4bd86ada06dac758fc761e
NShevtsov <neketobro@gamil.com>
2025-03-07 12:11:55 +0200

    create navigation

--- lesson_32/my-app/src/App.jsx
+++ lesson_32/my-app/src/App.jsx
@@ -14,18 +14,3 @@ function App() {
 }
 
 export default App
-
-// import { RouterProvider } from 'react-router-dom';
-// import { router } from './router/index'
-// import { Loader } from './components/Loader/Loader';
-
-// function App() {
-//   return (
-//     <RouterProvider
-//       router={router}
-//       fallbackElement={<Loader />}
-//       future={{ v7_startTransition: true }}
-//     />
-//   )
-// }
-// export default App;
--- lesson_32/my-app/src/components/Header/Navigation/Navigation.jsx
+++ lesson_32/my-app/src/components/Header/Navigation/Navigation.jsx
@@ -1,20 +1,26 @@
 import { Box, Tab, Tabs } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export function Navigation() {
+    const location = useLocation();
+
+    const tabMapping = {
+        '/': 0,
+        '/todolist': 1,
+        '/swapi': 2
+    };
+
+    const currentTab = tabMapping[location.pathname] ?? false;
 
     return (
-        <Box>
+        <Box sx={{ width: '100%' }}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                {/* <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                    <Tab label="Item One" {...a11yProps(0)} />
-                    <Tab label="Item Two" {...a11yProps(1)} />
-                    <Tab label="Item Three" {...a11yProps(2)} />
-                </Tabs> */}
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/todolist">Todo List</NavLink>
-                <NavLink to="/swapi">SWAPI</NavLink>
+                <Tabs value={currentTab} aria-label="navigation tabs">
+                    <Tab label="Home" component={NavLink} to="/" />
+                    <Tab label="Todo List" component={NavLink} to="/todolist" />
+                    <Tab label="SWAPI" component={NavLink} to="/swapi" />
+                </Tabs>
             </Box>
         </Box>
-    )
+    );
 }
